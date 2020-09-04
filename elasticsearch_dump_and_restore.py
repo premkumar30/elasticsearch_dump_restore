8b4b15ec192fc2e7c79c162fa76a8848160085bf
premkumarchalmeti <premkumar.chalmeti@incentius.com>
2020-09-04 15:07:59 +0530

    structure added

--- elasticsearch_dump_and_restore.py
+++ elasticsearch_dump_and_restore.py
@@ -1,20 +1,14 @@
 
 INDICES = [
-    "discovery_org_entities_v1",
-    "discovery_log_v3",
-    "discovery_log_admin",
-    "discovery_chat",
-    "revised_email_stats",
-    "all_individual_v7",
-    "all_practices_v1",
-    "all_individual",
-    "all_formulary_document_v10",
-    'daily_stats_rollup',
-    'discovery_email_events_v1',
-    'local_notifications',
-    "discovery_impersonate_index"
+        "documents_index",
+        "england_geographies"
 ]
 
+SRC_HOST = 'src_host'
+SRC_PORT = 9200
+
+DEST_HOST = 'dest_host'
+DEST_PORT = 9200
 
 MAPPING_CMD = 'elasticdump --input=http://%(SRC_HOST)s:%(SRC_PORT)s/%(INDEX)s --output=http://%(DEST_HOST)s:%(DEST_PORT)s/%(INDEX)s --type=mapping'
 DATA_CMD = 'elasticdump --input=http://%(SRC_HOST)s:%(SRC_PORT)s/%(INDEX)s --output=http://%(DEST_HOST)s:%(DEST_PORT)s/%(INDEX)s --type=data'
@@ -23,10 +17,10 @@ DATA_CMD = 'elasticdump --input=http://%(SRC_HOST)s:%(SRC_PORT)s/%(INDEX)s --out
 for index in INDICES:
     os.system(
         MAPPING_CMD.format({
-            'SRC_HOST': 'localhost',
-            'SRC_PORT': 9200,
-            'DEST_HOST': '192.168.11.22',
-            'DEST_PORT': 9200,
+            'SRC_HOST': SRC_HOST,
+            'SRC_PORT': SRC_PORT,
+            'DEST_HOST': DEST_HOST,
+            'DEST_PORT': DEST_PORT,
             'INDEX': index
         })
     )
